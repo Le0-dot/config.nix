0c09bca7c7e60cd2deb62ec145be5f8bcfbc5b55
Lev Koliadich <lev.koliadich@kyriba.com>
2025-08-16 18:04:58 +0200

    Create module for scripts

---
+++ home/scripts/playerctl.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [
+    (pkgs.writeShellScriptBin "playerctl-save" ''
+      #!/usr/bin/env sh
+
+      file=~/.player
+
+      function status() {
+          playerctl -a metadata -f '{{ playerName }} {{ status }}'
+      }
+
+      function status-to-script() {
+          sed '/Paused/d; /Stopped/d; s/^\(.\+\) Playing/playerctl -p \1 play/'
+      }
+
+      status | status-to-script > $file
+    '')
+    (pkgs.writeShellScriptBin "playerctl-resume" ''
+      #!/usr/bin/env sh
+
+      file=~/.player
+
+      sh $file && rm $file
+    '')
+  ];
+}
--- home/wm/hyprland.nix
+++ home/wm/hyprland.nix
@@ -33,28 +33,6 @@ in {
       playerctl
       networkmanagerapplet
       sway-contrib.grimshot
-      (pkgs.writeShellScriptBin "playerctl-save" ''
-        #!/usr/bin/env sh
-
-        file=~/.player
-
-        function status() {
-            playerctl -a metadata -f '{{ playerName }} {{ status }}'
-        }
-
-        function status-to-script() {
-            sed '/Paused/d; /Stopped/d; s/^\(.\+\) Playing/playerctl -p \1 play/'
-        }
-
-        status | status-to-script > $file
-      '')
-      (pkgs.writeShellScriptBin "playerctl-resume" ''
-        #!/usr/bin/env sh
-
-        file=~/.player
-
-        sh $file && rm $file
-      '')
     ];
 
     home.pointerCursor = {
