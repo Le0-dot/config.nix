62385260bcccd9d8ee7efd3ae4ea80bebebdea28
Lev Koliadich <lev.koliadich@kyriba.com>
2025-08-26 16:16:42 +0200

    Add formatter

--- flake.nix
+++ flake.nix
@@ -63,6 +63,13 @@
           ./home
         ];
       };
-      formatter."${system}" = pkgs.nixfmt;
+      formatter.${system} = pkgs.treefmt.withConfig {
+        runtimeInputs = with pkgs; [
+          nixfmt
+          deadnix
+          keep-sorted
+        ];
+        settings = pkgs.lib.importTOML ./treefmt.toml;
+      };
     };
 }
