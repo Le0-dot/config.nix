1a05b624b32b379bc5dcf1edaaad485be893ccd6
Lev Koliadich <lev.koliadich@kyriba.com>
2025-08-29 17:11:24 +0200

    Closes #8

--- home/features/desktop/kanshi.nix
+++ home/features/desktop/kanshi.nix
@@ -1,4 +1,3 @@
-# TODO: Try setting docked with builtin and any 2 displays
 { lib, config, ... }:
 
 {
@@ -19,62 +18,20 @@
           profile.outputs = [ { criteria = "$BUILTIN"; } ];
         }
         {
-          profile.name = "docked1";
+          profile.name = "docked";
           profile.outputs = [
             {
               criteria = "$BUILTIN";
               position = "960,1080";
             }
             {
-              criteria = "Dell Inc. DELL P2317H V2G517BE336L";
+              criteria = "*";
               position = "0,0";
               mode = "1920x1080@60";
               scale = 1.0;
             }
             {
-              criteria = "Dell Inc. DELL P2317H V2G517BE331L";
-              position = "1920,0";
-              mode = "1920x1080@60";
-              scale = 1.0;
-            }
-          ];
-        }
-        {
-          profile.name = "docked2";
-          profile.outputs = [
-            {
-              criteria = "$BUILTIN";
-              position = "960,1080";
-            }
-            {
-              criteria = "Dell Inc. DELL P2319H C2J9RS2";
-              position = "0,0";
-              mode = "1920x1080@60";
-              scale = 1.0;
-            }
-            {
-              criteria = "Dell Inc. DELL P2319H CKW45R2";
-              position = "1920,0";
-              mode = "1920x1080@60";
-              scale = 1.0;
-            }
-          ];
-        }
-        {
-          profile.name = "docked3";
-          profile.outputs = [
-            {
-              criteria = "$BUILTIN";
-              position = "960,1080";
-            }
-            {
-              criteria = "Dell Inc. DELL P2317H 9KFDW757216I";
-              position = "0,0";
-              mode = "1920x1080@60";
-              scale = 1.0;
-            }
-            {
-              criteria = "Dell Inc. DELL P2317H V2G517BAB9FL";
+              criteria = "**";
               position = "1920,0";
               mode = "1920x1080@60";
               scale = 1.0;
