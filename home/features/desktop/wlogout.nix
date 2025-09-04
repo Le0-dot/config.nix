c70f0a99e7c074570c952fc137aa2853c724a117
Lev Koliadich <lev.koliadich@kyriba.com>
2025-09-04 16:58:08 +0200

    Closes #12. Noe really created stylix module, but used stylix defined colors and style guide, so if theme is changed should look fine.

--- home/features/desktop/wlogout.nix
+++ home/features/desktop/wlogout.nix
@@ -1,4 +1,3 @@
-# TODO: Write stylix module???
 {
   pkgs,
   lib,
@@ -39,6 +38,23 @@
         }
       ];
       style = ''
+        @define-color base00 #${config.lib.stylix.colors.base00};
+        @define-color base01 #${config.lib.stylix.colors.base01};
+        @define-color base02 #${config.lib.stylix.colors.base02};
+        @define-color base03 #${config.lib.stylix.colors.base03};
+        @define-color base04 #${config.lib.stylix.colors.base04};
+        @define-color base05 #${config.lib.stylix.colors.base05};
+        @define-color base06 #${config.lib.stylix.colors.base06};
+        @define-color base07 #${config.lib.stylix.colors.base07};
+        @define-color base08 #${config.lib.stylix.colors.base08};
+        @define-color base09 #${config.lib.stylix.colors.base09};
+        @define-color base0A #${config.lib.stylix.colors.base0A};
+        @define-color base0B #${config.lib.stylix.colors.base0B};
+        @define-color base0C #${config.lib.stylix.colors.base0C};
+        @define-color base0D #${config.lib.stylix.colors.base0D};
+        @define-color base0E #${config.lib.stylix.colors.base0E};
+        @define-color base0F #${config.lib.stylix.colors.base0F};
+
         * {
             background-image: none;
             box-shadow: none;
@@ -47,24 +63,25 @@
         window {
             font-family: monospace;
             font-size: 14pt;
-            color: #cad3f5; /* @base05 */
-            background-color: rgba(54, 58, 79, 0.5); /* @base02 + 0.5 alpha */
+            color: @base05;
+            background-color: alpha(@base01, 0.5);
         }
 
         button {
             background-repeat: no-repeat;
             background-position: center;
             background-size: 25%;
-            background-color: rgba(54, 58, 79, 0.5); /* @base02 + 0.5 alpha */
-            color: #cad3f5; /* @base05 */
-            border-color: #8aadf4; /* @base0D */
+            background-color: alpha(@base00, 0.75);
+            color: @base05;
+            border-color: @base03;
             border-width: 0.2rem;
             border-radius: 2rem;
             margin: 10px;
         }
 
         button:focus, button:active, button:hover {
-            background-color: #5b6078; /* @base04 */
+            background-color: @base02;
+            border-color: @base0D;
             outline-style: none;
         }
 
