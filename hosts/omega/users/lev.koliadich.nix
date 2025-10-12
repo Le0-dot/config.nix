61fc17d6a83b6ef736f45dc05d89db9835768932
Lev Koliadich <lev.koliadich@kyriba.com>
2025-10-12 16:35:11 +0200

    Add git home module with sensible defaults

--- hosts/omega/users/lev.koliadich.nix
+++ hosts/omega/users/lev.koliadich.nix
@@ -1,7 +1,17 @@
+{ flake, ... }:
+
 {
+  imports = [ flake.homeModules.git ];
+
   config = {
     home.username = "lev.koliadich";
     home.homeDirectory = "/home/lev.koliadich";
     home.stateVersion = "25.05";
+
+    programs.git = {
+      enable = true;
+      userName = "Lev Koliadich";
+      userEmail = "lkolyadich@gmail.com";
+    };
   };
 }
---
+++ modules/home/git.nix
@@ -0,0 +1,18 @@
+{ lib, config, ... }:
+
+{
+  config = lib.mkIf config.programs.git.enable {
+    programs.git = {
+      ignores = [
+        "Session.vim"
+        ".envrc"
+      ];
+      extraConfig = {
+        init.defaultBranch = "main";
+        core.autocrlf = false;
+        core.rebase = true;
+        pull.rebase = true;
+      };
+    };
+  };
+}
