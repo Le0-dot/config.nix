4fbd5ddfdafb8d9563e6c44843aa3e6e7a0884ea
Lev Koliadich <lev.koliadich@kyriba.com>
2025-10-16 15:48:48 +0200

    Remove everything

--- system/pam.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-  environment.etc."pam.d/hyprlock".text = ''
-    auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so max_tries=3
-    auth sufficient ${pkgs.sssd}/lib/security/pam_sss.so try_first_pass
-    auth required ${pkgs.linux-pam}/lib/security/pam_deny.so
-  '';
-}
