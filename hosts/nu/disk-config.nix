05ed8465947888407cca85e656212318f4c4e818
Lev Koliadich <lkolyadich@gmail.com>
2025-11-30 18:00:42 +0100

    add raid1 config for data disks

--- hosts/nu/disk-config.nix
+++ hosts/nu/disk-config.nix
@@ -1,8 +1,13 @@
+let
+  root_disk = "/dev/disk/by-id/nvme-Lexar_SSD_NM620_512GB_QFB155R004190P110W";
+  data_disk1 = "/dev/disk/by-id/nvme-ADATA_LEGEND_900_4N4221121212";
+  data_disk2 = "/dev/disk/by-id/nvme-CT2000P3PSSD8_2516E9B84F3A";
+in
 {
   disko.devices.disk = {
     main = {
       type = "disk";
-      device = "/dev/disk/by-id/nvme-Lexar_SSD_NM620_512GB_QFB155R004190P110W";
+      device = root_disk;
       content = {
         type = "gpt";
         partitions = {
@@ -43,6 +48,29 @@
                 };
               };
             };
+          };
+        };
+      };
+    };
+    data = {
+      type = "disk";
+      device = data_disk1;
+      content = {
+        type = "btrfs";
+        extraArgs = [
+          "-f"
+          "-m raid1"
+          "-d raid1"
+          data_disk2
+        ];
+        subvolumes = {
+          "/data" = {
+            mountpoint = "/srv/data";
+            mountOptions = [
+              "compress=zstd"
+              "noatime"
+              "ssd"
+            ];
           };
         };
       };
