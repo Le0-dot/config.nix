f9a993af8ae400dc6c121219e545ba7be7106106
Lev Koliadich <lkolyadich@gmail.com>
2025-12-02 15:17:49 +0100

    add removal of non-existant container services

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -73,9 +73,28 @@ in
     #     ${lib.getExe pkgs.tailscale} serve set-config --all ${configFile}
     #   '';
 
-    system.activationScripts.tailscale-serve-service-containers = builtins.concatStringsSep "\n" (
-      [ "${lib.getExe pkgs.tailscale} serve reset" ]
-      ++ lib.attrsets.mapAttrsToList containerToServe (
+    system.activationScripts.tailscale-serve-clear-container-services =
+      let
+        desired-services = builtins.concatStringsSep "\n" (
+          map (service: "svc:${service}") (
+            builtins.attrNames (
+              lib.attrsets.filterAttrs notEmptyPorts config.virtualisation.oci-containers.containers
+            )
+          )
+        );
+      in
+      ''
+        existing=$(${lib.getExe pkgs.tailscale} serve status -json | ${lib.getExe pkgs.jq} -r '.Services | keys[]')
+        desired="${desired-services}"
+
+        clear=$(comm -23 <(printf '%s\n' "$existing" | sort) <(printf '%s\n' "$desired" | sort))
+        for service in $clear; do
+          ${lib.getExe pkgs.tailscale} serve clear "$service"
+        done
+      '';
+
+    system.activationScripts.tailscale-serve-set-container-services = builtins.concatStringsSep "\n" (
+      lib.attrsets.mapAttrsToList containerToServe (
         lib.attrsets.filterAttrs notEmptyPorts config.virtualisation.oci-containers.containers
       )
     );
