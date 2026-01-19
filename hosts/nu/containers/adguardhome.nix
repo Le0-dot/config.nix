028de5cffca1d4959af7a510ffa1477a8ca2539b
Lev Koliadich <lkolyadich@gmail.com>
2026-01-19 16:05:10 +0100

    dynalmically set tailscale ip for the adguardhome to bind to

--- hosts/nu/containers/adguardhome.nix
+++ hosts/nu/containers/adguardhome.nix
@@ -1,22 +1,50 @@
-{ flake, config, ... }:
+{
+  pkgs,
+  flake,
+  config,
+  ...
+}:
 
 let
   btrfsVolume = flake.lib.btrfsVolume config.disko;
   mountVolume = flake.lib.mountVolume;
-  tailcaleIp = "100.83.204.23";
+  envFile = "/run/containers/environment/tailscale-ip";
 in
 {
+  systemd.services.tailscale-env = {
+    description = "Export Tailscale IP to environment file";
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    after = [ "tailscaled.service" ];
+    requires = [ "tailscaled.service" ];
+    path = [ pkgs.tailscale ];
+    preStart = "until tailscale status; do sleep 10; done";
+    script = ''
+      mkdir -p $(dirname ${envFile})
+      echo "TAILSCALE_IP=$(tailscale ip -4)" > ${envFile}
+    '';
+  };
+
   virtualisation.quadlet =
     let
       inherit (config.virtualisation.quadlet) pods volumes;
     in
     {
       pods.adguardhome = {
+        unitConfig = {
+          After = [ "tailscale-env.service" ];
+          Requires = [ "tailscale-env.service" ];
+        };
+        serviceConfig = {
+          EnvironmentFile = envFile;
+        };
         podConfig = {
           publishPorts = [
             "3000:3000"
-            "${tailcaleIp}:53:53/udp"
-            "${tailcaleIp}:53:53/tcp"
+            "\${TAILSCALE_IP}:53:53/udp"
+            "\${TAILSCALE_IP}:53:53/tcp"
           ];
           labels = {
             "tailscale.service.adguardhome.https" = "3000";
