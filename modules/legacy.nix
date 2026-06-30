38a42d43182dc79d14b27ce0bdced3e0e0a84af8
Lev Koliadich <lkolyadich@gmail.com>
2026-06-30 17:20:41 +0200

    A4: create modules/legacy.nix - temporary bridge
    Bridge existing host configs into den's flake output system.
    Produces nixosConfigurations.{tau,nu}, systemConfigurations.omega,
    and homeConfigurations.lev.koliadich@omega using the original
    host files with replicated specialArgs (flake.lib, perSystem.self).
    
    This is temporary - will be removed as hosts migrate to aspects.

---
+++ modules/legacy.nix
@@ -0,0 +1,61 @@
+{ inputs, ... }:
+
+let
+  system = "x86_64-linux";
+  pkgs = inputs.nixpkgs.legacyPackages.${system};
+  customLib = import ../lib/default.nix { };
+
+  # Replicate what blueprint provided as specialArgs
+  flake = {
+    lib = customLib;
+  };
+
+  # Replicate perSystem.self.* for home modules that reference custom packages
+  perSystem = {
+    self = {
+      choose-repo = import ../packages/choose-repo.nix { inherit pkgs; };
+      clipselect = import ../packages/clipselect.nix { inherit pkgs; };
+      hyprlock = import ../packages/hyprlock.nix { inherit pkgs; };
+      hyprpaper = import ../packages/hyprpaper.nix { inherit pkgs; };
+    };
+  };
+in
+{
+  flake.nixosConfigurations.tau = inputs.nixpkgs.lib.nixosSystem {
+    modules = [ ../hosts/tau/configuration.nix ];
+    specialArgs = {
+      inherit inputs flake;
+      hostName = "tau";
+    };
+  };
+
+  flake.nixosConfigurations.nu = inputs.nixpkgs.lib.nixosSystem {
+    modules = [ ../hosts/nu/configuration.nix ];
+    specialArgs = {
+      inherit inputs flake;
+      hostName = "nu";
+    };
+  };
+
+  flake.systemConfigurations.omega = inputs.system-manager.lib.makeSystemConfig {
+    modules = [ ../hosts/omega/system-configuration.nix ];
+    extraSpecialArgs = { inherit inputs; };
+  };
+
+  flake.homeConfigurations."lev.koliadich@omega" =
+    inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      modules = [
+        ../hosts/omega/users/lev.koliadich.nix
+        {
+          home.username = "lev.koliadich";
+          home.homeDirectory = "/home/lev.koliadich";
+          home.stateVersion = "26.05";
+        }
+      ];
+      extraSpecialArgs = { inherit inputs perSystem; };
+    };
+}
