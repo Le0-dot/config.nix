a954207f7b60fcd9fc5ce4df0b722d7cca6e585f
Lev Koliadich <lkolyadich@gmail.com>
2026-07-29 17:27:56 +0200

    align with den branch packages

--- packages/hyprlock.nix
+++ packages/hyprlock.nix
@@ -2,11 +2,6 @@
 
 pkgs.writeShellApplication {
   name = "hyprlock";
-  runtimeInputs = [
-    pkgs.hyprlock
-  ];
-  text = ''
-    export LD_PRELOAD="${pkgs.sssd}/lib/libnss_sss.so.2"
-    hyprlock "$@"
-  '';
+  runtimeInputs = [ pkgs.hyprlock ];
+  text = ''exec env LD_PRELOAD="${pkgs.sssd}/lib/libnss_sss.so.2" hyprlock "$@"'';
 }
--- packages/hyprpaper.nix
+++ packages/hyprpaper.nix
@@ -2,11 +2,6 @@
 
 pkgs.writeShellApplication {
   name = "hyprpaper";
-  runtimeInputs = [
-    pkgs.hyprpaper
-  ];
-  text = ''
-    export LD_PRELOAD="${pkgs.sssd}/lib/libnss_sss.so.2"
-    hyprpaper "$@"
-  '';
+  runtimeInputs = [ pkgs.hyprpaper ];
+  text = ''exec env LD_PRELOAD="${pkgs.sssd}/lib/libnss_sss.so.2" hyprpaper "$@"'';
 }
