c2950876b28de7c9b299e97ac4c42f2d331b6f92
Lev Koliadich <lkolyadich@gmail.com>
2026-07-30 16:49:22 +0200

    merge tau disko config into it's main config file

--- modules/hosts/tau/default.nix
+++ modules/hosts/tau/default.nix
@@ -15,10 +15,7 @@
         ...
       }:
       {
-        imports = [
-          inputs.nixpkgs.nixosModules.notDetected
-          ./_disk.nix
-        ];
+        imports = [ inputs.nixpkgs.nixosModules.notDetected ];
 
         hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
 
@@ -30,6 +27,39 @@
         boot.initrd.kernelModules = [ ];
         boot.kernelModules = [ "kvm-intel" ];
         boot.extraModulePackages = [ ];
+
+        disko.devices.disk.main = {
+          type = "disk";
+          device = "/dev/sda";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                type = "EF00";
+                size = "1G";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "defaults" ];
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    root = {
+                      mountpoint = "/";
+                      mountOptions = [ "noatime" ];
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
 
         # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
         # (the default) this is the recommended approach. When using systemd-networkd it's
