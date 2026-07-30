1584b27f0450d71a2430d1da60dd33151f00eb4c
Lev Koliadich <lkolyadich@gmail.com>
2026-07-30 17:01:52 +0200

    merge nu hardware config into it's main config file

--- modules/hosts/nu/default.nix
+++ modules/hosts/nu/default.nix
@@ -28,33 +28,59 @@
       den.aspects.server.containers.sonarr
       den.aspects.server.containers.transmission
     ];
-    nixos = { config, pkgs, ... }: {
-      imports = [
-        inputs.btr-backup.nixosModules.btr-backup
-        ./_hardware.nix
-        ./_disk.nix
-        ./_secrets.nix
-        ./_backup.nix
-      ];
-
-      _module.args.flake = self;
-
-      environment.systemPackages = [
-        pkgs.neovim
-        pkgs.curl
-        pkgs.jq
-      ];
-
-      services.tailscale = {
-        enable = true;
-        authKeyFile = config.age.secrets.tailscale-key.path;
-        authKeyParameters.ephemeral = false;
-        extraUpFlags = [
-          "--advertise-tags=tag:nix"
-          "--ssh"
+    nixos =
+      {
+        lib,
+        pkgs,
+        config,
+        ...
+      }:
+      {
+        imports = [
+          inputs.nixpkgs.nixosModules.notDetected
+          inputs.btr-backup.nixosModules.btr-backup
+          ./_hardware.nix
+          ./_disk.nix
+          ./_secrets.nix
+          ./_backup.nix
         ];
-        services.enable = true;
+
+        hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+
+        boot.initrd.availableKernelModules = [
+          "xhci_pci"
+          "ahci"
+          "nvme"
+          "sdhci_pci"
+        ];
+        boot.initrd.kernelModules = [ ];
+        boot.kernelModules = [ "kvm-intel" ];
+        boot.extraModulePackages = [ ];
+
+        # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+        # (the default) this is the recommended approach. When using systemd-networkd it's
+        # still possible to use this option, but it's recommended to use it in conjunction
+        # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+        networking.useDHCP = lib.mkDefault true;
+        # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+        # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
+
+        environment.systemPackages = [
+          pkgs.neovim
+          pkgs.curl
+          pkgs.jq
+        ];
+
+        services.tailscale = {
+          enable = true;
+          authKeyFile = config.age.secrets.tailscale-key.path;
+          authKeyParameters.ephemeral = false;
+          extraUpFlags = [
+            "--advertise-tags=tag:nix"
+            "--ssh"
+          ];
+          services.enable = true;
+        };
       };
-    };
   };
 }
