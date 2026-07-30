ab0d982df57ec7d8423ef1476bb7c447e404cdcf
Lev Koliadich <lkolyadich@gmail.com>
2026-07-30 14:02:05 +0200

    fix

--- modules/aspects/desktop/default.nix
+++ modules/aspects/desktop/default.nix
@@ -4,7 +4,6 @@
     includes = [
       den.aspects.desktop.uwsm
       den.aspects.desktop.kanshi # TODO convert kashi config to pure hyprland lua config
-      den.aspects.desktop.dunst
       den.aspects.desktop.tofi
       den.aspects.desktop.waybar
       den.aspects.desktop.wlogout
@@ -139,13 +138,13 @@
         };
       };
     };
-  };
 
-  systemManager = { pkgs, ... }: {
-    environment.etc."pam.d/hyprlock".text = ''
-      auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so max_tries=3
-      auth sufficient ${pkgs.sssd}/lib/security/pam_sss.so try_first_pass
-      auth required ${pkgs.linux-pam}/lib/security/pam_deny.so
-    '';
+    systemManager = { pkgs, ... }: {
+      environment.etc."pam.d/hyprlock".text = ''
+        auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so max_tries=3
+        auth sufficient ${pkgs.sssd}/lib/security/pam_sss.so try_first_pass
+        auth required ${pkgs.linux-pam}/lib/security/pam_deny.so
+      '';
+    };
   };
 }
