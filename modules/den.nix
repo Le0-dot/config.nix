e37336f134790d796a0a0ea3fb7dce2a6fe36352
Lev Koliadich <lkolyadich@gmail.com>
2026-07-31 12:31:25 +0200

    remove users and homes

--- modules/den.nix
+++ modules/den.nix
@@ -35,11 +35,11 @@
     };
   };
 
-  den.hosts.x86_64-linux.nu.users."lev.koliadich" = { };
-  den.hosts.x86_64-linux.tau.users."lev.koliadich" = { };
+  den.hosts.x86_64-linux.nu = { };
+  den.hosts.x86_64-linux.tau = { };
   den.hosts.x86_64-linux.omega.class = "systemManager";
 
-  den.homes.x86_64-linux."lev.koliadich@nu" = { };
-  den.homes.x86_64-linux."lev.koliadich@tau" = { };
+  # den.homes.x86_64-linux."lev.koliadich@nu" = { };
+  # den.homes.x86_64-linux."lev.koliadich@tau" = { };
   den.homes.x86_64-linux."lev.koliadich@omega" = { };
 }
