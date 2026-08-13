7300d9bdffe4e2fe92a9fadbcf850eb5643eee30
Lev Koliadich <lkolyadich@gmail.com>
2026-08-13 18:43:48 +0200

    make deploy-nixos aspect self-contained

--- modules/policies/deploy.nix
+++ modules/policies/deploy.nix
@@ -5,25 +5,21 @@
   ...
 }:
 {
-  den.aspects.deploy-nixos = host: {
-    packages =
-      { pkgs, ... }:
-      {
-        "deploy-${host}" = pkgs.writeShellApplication {
-          name = "deploy-${host}";
-          runtimeInputs = [ pkgs.nixos-rebuild ];
-          text = ''
-            nixos-rebuild switch --flake ${self}#${host} --target-host ${host} --sudo "$@"
-          '';
-        };
+  den.aspects.deploy-nixos = { host, ... }: {
+    packages = { pkgs, ... }: {
+      "deploy-${host.hostName}" = pkgs.writeShellApplication {
+        name = "deploy-${host.hostName}";
+        runtimeInputs = [ pkgs.nixos-rebuild ];
+        text = ''
+          nixos-rebuild switch --flake ${self}#${host.hostName} --target-host ${host.hostName} --sudo "$@"
+        '';
       };
+    };
   };
 
   den.policies.deploy-nixos =
     { host, ... }:
-    den.lib.policy.include (
-      lib.optional (host.class == "nixos") (den.aspects.deploy-nixos host.hostName)
-    );
+    den.lib.policy.include (lib.optional (host.class == "nixos") den.aspects.deploy-nixos);
 
   den.schema.host.includes = [ den.policies.deploy-nixos ];
 }
