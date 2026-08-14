e47f5f0dcbd87e706e34a22ba7ec14e6b0c01e1a
Lev Koliadich <lkolyadich@gmail.com>
2026-08-14 13:34:12 +0200

    rename the switch packages, add boot packages for nixos and switch packages for system-maanger

--- modules/aspects/hosts/omega.nix
+++ modules/aspects/hosts/omega.nix
@@ -14,17 +14,6 @@
         "d /etc/profiles/per-user 0755 root root -"
       ];
     };
-    packages =
-      { pkgs, ... }:
-      {
-        switch-omega = pkgs.writeShellApplication {
-          name = "switch-omega";
-          runtimeInputs = [ pkgs.system-manager ];
-          text = ''
-            system-manager switch --flake ${self}#omega --sudo
-          '';
-        };
-      };
     checks =
       { lib, pkgs, ... }:
       let
---
+++ modules/policies/switch.nix
@@ -0,0 +1,72 @@
+{ den, self, ... }:
+{
+  den.aspects.switch-nixos =
+    { host, ... }:
+    {
+      packages =
+        { pkgs, ... }:
+        {
+          "switch-${host.hostName}" = pkgs.writeShellApplication {
+            name = "switch-${host.hostName}";
+            runtimeInputs = [ pkgs.nixos-rebuild ];
+            text = ''
+              nixos-rebuild switch --flake ${self}#${host.hostName} --sudo "$@"
+            '';
+          };
+          "remote-switch-${host.hostName}" = pkgs.writeShellApplication {
+            name = "remote-switch-${host.hostName}";
+            runtimeInputs = [ pkgs.nixos-rebuild ];
+            text = ''
+              nixos-rebuild switch --flake ${self}#${host.hostName} --target-host ${host.hostName} --sudo "$@"
+            '';
+          };
+          "boot-${host.hostName}" = pkgs.writeShellApplication {
+            name = "boot-${host.hostName}";
+            runtimeInputs = [ pkgs.nixos-rebuild ];
+            text = ''
+              nixos-rebuild boot --flake ${self}#${host.hostName} --sudo "$@"
+            '';
+          };
+          "remote-boot-${host.hostName}" = pkgs.writeShellApplication {
+            name = "remote-boot-${host.hostName}";
+            runtimeInputs = [ pkgs.nixos-rebuild ];
+            text = ''
+              nixos-rebuild boot --flake ${self}#${host.hostName} --target-host ${host.hostName} --sudo "$@"
+            '';
+          };
+        };
+    };
+
+  den.aspects.switch-system-manager = { host, ... }: {
+    packages =
+      { pkgs, ... }:
+      {
+        "switch-${host.hostName}" = pkgs.writeShellApplication {
+          name = "switch-${host.hostName}";
+          runtimeInputs = [ pkgs.system-manager ];
+          text = ''
+            system-manager switch --flake ${self}#${host.hostName} --sudo
+          '';
+        };
+        "remote-switch-${host.hostName}" = pkgs.writeShellApplication {
+          name = "remote-switch-${host.hostName}";
+          runtimeInputs = [ pkgs.system-manager ];
+          text = ''
+            system-manager switch --flake ${self}#${host.hostName} --target-host ${host.hostName} --sudo
+          '';
+        };
+      };
+  };
+
+  den.policies.switch =
+    { host, ... }:
+    let
+      class-aspects = {
+        nixos = den.aspects.switch-nixos;
+        systemManager = den.aspects.switch-system-manager;
+      };
+    in
+    den.lib.policy.include (class-aspects.${host.class} or { });
+
+  den.schema.host.includes = [ den.policies.switch ];
+}
