d913c8af6077c06c28dd559d3c25e9de33cfc15a
Lev Koliadich <lkolyadich@gmail.com>
2026-08-18 11:16:40 +0200

    all tilt to user

--- modules/aspects/users/lev.koliadich.nix
+++ modules/aspects/users/lev.koliadich.nix
@@ -21,21 +21,24 @@
       den.aspects.terminal.television
       (den.aspects.terminal.neovim "projects/config.nvim")
     ];
-    homeManager = {
-      programs.zsh = {
-        envExtra = ''
-          if [ -e /etc/profile.d/system-manager-path.sh ]; then
-            . /etc/profile.d/system-manager-path.sh
-          else
-            echo "Warning: /etc/profile.d/system-manager-path.sh not found. Please ensure that the system manager is installed and configured correctly."
-          fi
-        '';
-        loginExtra = ''
-          if uwsm check may-start && uwsm select; then 
-            exec uwsm start default
-          fi
-        '';
+    homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = [ pkgs.tilt ];
+        programs.zsh = {
+          envExtra = ''
+            if [ -e /etc/profile.d/system-manager-path.sh ]; then
+              . /etc/profile.d/system-manager-path.sh
+            else
+              echo "Warning: /etc/profile.d/system-manager-path.sh not found. Please ensure that the system manager is installed and configured correctly."
+            fi
+          '';
+          loginExtra = ''
+            if uwsm check may-start && uwsm select; then 
+              exec uwsm start default
+            fi
+          '';
+        };
       };
-    };
   };
 }
